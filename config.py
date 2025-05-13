9738930035e495d5286f68f5f58ec92cb4ca4239
tjbotz <tjbots7@gmail.com>
2025-05-13 10:48:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,8 +36,8 @@ FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/Itachi24x7")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 #--------------------------------------------
-START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
-FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
+START_PIC = os.environ.get("START_PIC", "https://envs.sh/RMM.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://envs.sh/JgK.jpg")
 #--------------------------------------------
 
 #--------------------------------------------
